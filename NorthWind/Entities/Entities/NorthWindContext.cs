aa4756a33db41ad91c7ae8d79b4cb1873bd00b1d
Dennis Serrano <dserrano10115@ufide.ac.cr>
2023-06-25 15:27:31 -0600

    Semana 05 - Mapeo SP

--- NorthWind/Entities/Entities/NorthWindContext.cs
+++ NorthWind/Entities/Entities/NorthWindContext.cs
@@ -43,13 +43,16 @@ namespace Entities.Entities
         public virtual DbSet<SummaryOfSalesByYear> SummaryOfSalesByYears { get; set; } = null!;
         public virtual DbSet<Supplier> Suppliers { get; set; } = null!;
         public virtual DbSet<Territory> Territories { get; set; } = null!;
+        public virtual DbSet<sp_GetAllCategories_Result> sp_GetAllCategories_Results { get; set; } = null!;
+
+
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=THE-DARKNIGHT\\SQLEXPRESS;Database=NorthWind;Integrated Security=True;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("Server=.;Database=NorthWind;Integrated Security=True;Trusted_Connection=True;");
             }
         }
 
