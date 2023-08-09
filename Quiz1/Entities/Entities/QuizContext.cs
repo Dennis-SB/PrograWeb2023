f4bb855d03aea0dfd0f5bdb6d96aeb658b1bdc54
Dennis Serrano <dserrano10115@ufide.ac.cr>
2023-08-08 18:59:46 -0600

    Quiz2 - API Key Completado

--- Quiz1/Entities/Entities/QuizContext.cs
+++ Quiz1/Entities/Entities/QuizContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Entities.Utilities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 
@@ -9,26 +10,24 @@ namespace Entities.Entities
     {
         public QuizContext()
         {
+            var optionBuilder = new DbContextOptionsBuilder<QuizContext>();
+            optionBuilder.UseSqlServer(Util.ConnectionString);
         }
-
         public QuizContext(DbContextOptions<QuizContext> options)
             : base(options)
         {
         }
-
         public virtual DbSet<Empleado> Empleados { get; set; } = null!;
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=THE-DARKNIGHT\\SQLEXPRESS;Database=Quiz;Integrated Security=True;Trusted_Connection=True;");
-            }
+            optionsBuilder.UseSqlServer(Util.ConnectionString);
+            base.OnConfiguring(optionsBuilder);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
             modelBuilder.UseCollation("Modern_Spanish_CI_AS");
 
             modelBuilder.Entity<Empleado>(entity =>
